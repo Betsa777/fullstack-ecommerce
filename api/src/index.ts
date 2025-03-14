03e23836b55b99c85da48af120ae6bc3418f8a7f
DevBlock777 <dakuyobetsaleel84@gmail.com>
2025-03-14 20:48:46 +0000

    Products router setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express"
+import productRoutes from "./routes/products/index"
 const app = express()
 const port:number = 3000
 
 app.get("/",(req,res)=>{
     res.send("Hello world 123")
 })
+
+app.use('/products',productRoutes)
 
 app.listen(port,()=>{
     console.log("Example chat on 3000")
