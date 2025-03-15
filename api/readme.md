b3cbbf12bd2b54c67a3c2f1794aa2860085305b9
DevBlock777 <dakuyobetsaleel84@gmail.com>
2025-03-15 13:57:45 +0000

    json parsing middleware

---
+++ api/readme.md
@@ -0,0 +1,2 @@
+1. Create a specific route for all the data referred on the same object
+2. You can add controllers which will contain the logic to handle the requests
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express,{json,urlencoded} from "express"
 import productRoutes from "./routes/products/index"
 const app = express()
 const port:number = 3000
@@ -6,7 +6,8 @@ const port:number = 3000
 app.get("/",(req,res)=>{
     res.send("Hello world 123")
 })
-
+app.use(urlencoded({extended: false}))
+app.use(json())
 app.use('/products',productRoutes)
 
 app.listen(port,()=>{
